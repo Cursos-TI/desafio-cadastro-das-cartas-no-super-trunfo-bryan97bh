d0c5225cf27cafea5ffca6346ee05c94593108bc
bryan <bryan.matos97@gmail.com>
2025-04-05 21:26:42 -0300

    Exibição dos dados da Carta 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -80,6 +80,15 @@ printf("Área: %.2f km²\n", area1);
 printf("PIB: %.2f bilhões de reais\n", pib1);
 printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
 
+// Exibição dos dados da Carta 2
+printf("\nCarta 2:\n");
+printf("Estado: %c\n", estado2);
+printf("Código: %s\n", codigo2);
+printf("Nome da Cidade: %s\n", nomeCidade2);
+printf("População: %d\n", populacao2);
+printf("Área: %.2f km²\n", area2);
+printf("PIB: %.2f bilhões de reais\n", pib2);
+printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
 
     return 0;
 }
