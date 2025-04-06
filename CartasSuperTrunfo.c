d37afe28653871f60ad5ed1950b7481391748b06
bryan <bryan.matos97@gmail.com>
2025-04-05 22:58:27 -0300

    retirado os acentos pq tava bugando a escrita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -30,22 +30,22 @@ printf("Cadastro da Carta 1:\n");
 printf("Digite o estado (A a H): ");
 scanf(" %c", &estado1);
 
-printf("Digite o código da carta (ex: A01): ");
+printf("Digite o codigo da carta (ex: A01): ");
 scanf("%s", codigo1);
 
 printf("Digite o nome da cidade: ");
 scanf(" %[^\n]", nomeCidade1);  // lê até a nova linha
 
-printf("Digite a população da cidade: ");
+printf("Digite a populacao da cidade: ");
 scanf("%d", &populacao1);
 
-printf("Digite a área da cidade (em km²): ");
+printf("Digite a area da cidade (em km): ");
 scanf("%f", &area1);
 
-printf("Digite o PIB da cidade (em bilhões de reais): ");
+printf("Digite o PIB da cidade (em bilhoes de reais): ");
 scanf("%f", &pib1);
 
-printf("Digite o número de pontos turísticos: ");
+printf("Digite o numero de pontos turisticos: ");
 scanf("%d", &pontosTuristicos1);
 // Leitura dos dados da Carta 2
 printf("\nCadastro da Carta 2:\n");
@@ -59,37 +59,37 @@ scanf("%s", codigo2);
 printf("Digite o nome da cidade: ");
 scanf(" %[^\n]", nomeCidade2);
 
-printf("Digite a população da cidade: ");
+printf("Digite a populaçao da cidade: ");
 scanf("%d", &populacao2);
 
-printf("Digite a área da cidade (em km²): ");
+printf("Digite a area da cidade (em km²): ");
 scanf("%f", &area2);
 
-printf("Digite o PIB da cidade (em bilhões de reais): ");
+printf("Digite o PIB da cidade (em bilhoes de reais): ");
 scanf("%f", &pib2);
 
-printf("Digite o número de pontos turísticos: ");
+printf("Digite o numero de pontos turisticos: ");
 scanf("%d", &pontosTuristicos2);
 
 // Exibição dos dados da Carta 1
 printf("\nCarta 1:\n");
 printf("Estado: %c\n", estado1);
-printf("Código: %s\n", codigo1);
+printf("Codigo: %s\n", codigo1);
 printf("Nome da Cidade: %s\n", nomeCidade1);
-printf("População: %d\n", populacao1);
-printf("Área: %.2f km²\n", area1);
-printf("PIB: %.2f bilhões de reais\n", pib1);
-printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+printf("Populacao: %d\n", populacao1);
+printf("Area: %.2f km\n", area1);
+printf("PIB: %.2f bilhoes de reais\n", pib1);
+printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos1);
 
 // Exibição dos dados da Carta 2
 printf("\nCarta 2:\n");
 printf("Estado: %c\n", estado2);
-printf("Código: %s\n", codigo2);
+printf("Codigo: %s\n", codigo2);
 printf("Nome da Cidade: %s\n", nomeCidade2);
-printf("População: %d\n", populacao2);
-printf("Área: %.2f km²\n", area2);
-printf("PIB: %.2f bilhões de reais\n", pib2);
-printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+printf("Populacao: %d\n", populacao2);
+printf("Area: %.2f km\n", area2);
+printf("PIB: %.2f bilhoes de reais\n", pib2);
+printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos2);
 
 return 0;
 }
