fd6336b098ade08ce640494fef6d87209b48b492
bryan <bryan.matos97@gmail.com>
2025-04-05 21:27:52 -0300

    correcoes

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,22 +7,23 @@
 //Teste larissa
 
 int main() {
-    // Declaração das variáveis para a carta 1
-    char estado1;
-    char codigo1[4];
-    char nomeCidade1[50];
-    int populacao1;
-    float area1;
-    float pib1;
-    int pontosTuristicos1;
-    // Declaração das variáveis para a carta 2
-    char estado2;
-    char codigo2[4];
-    char nomeCidade2[50];
-    int populacao2;
-    float area2;
-    float pib2;
-    int pontosTuristicos2;
+// Declaração das variáveis para a carta 1
+char estado1;
+char codigo1[4];
+char nomeCidade1[50];
+int populacao1;
+float area1;
+float pib1;
+int pontosTuristicos1;
+// Declaração das variáveis para a carta 2
+char estado2;
+char codigo2[4];
+char nomeCidade2[50];
+int populacao2;
+float area2;
+float pib2;
+int pontosTuristicos2;
+    
 // Leitura dos dados da Carta 1
 printf("Cadastro da Carta 1:\n");
 
@@ -90,5 +91,5 @@ printf("Área: %.2f km²\n", area2);
 printf("PIB: %.2f bilhões de reais\n", pib2);
 printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
 
-    return 0;
+return 0;
 }
