c9f79be912c09e6ce06e25674a46766a29c4177d
bryan <bryan.matos97@gmail.com>
2025-04-05 21:24:33 -0300

    variavel carta 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -15,6 +15,13 @@ int main() {
     float area1;
     float pib1;
     int pontosTuristicos1;
-    
+    // Declaração das variáveis para a carta 2
+    char estado2;
+    char codigo2[4];
+    char nomeCidade2[50];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontosTuristicos2;
     return 0;
 }
